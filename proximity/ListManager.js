af00ae4bac3263ed9e79d854e4f4a719e1a50db8
hadi <modarres.zadeh@gmail.com>
2019-09-11 16:08:07 +1000

    list manager

--- proximity/ListManager.js
+++ proximity/ListManager.js
@@ -1,30 +1,62 @@
+let ProximityList = require("./ProximityList");
+
 class ListManager {
-    constructor(){
-        this.proximityListSize= 5;
+    constructor() {
+        this.proximityListSize = 5;
         this.lists = [];
     }
-    addGlobalList(globalList,proximityFunction){
-        for(let list of this.lists){
-            if (list.listName==="globalList"){
+
+    addGlobalList(globalList, proximityFunction) {
+        for (let list of this.lists) {
+            if (list.listName === "globalList") {
                 throw new Error(`global list ${globalList} already exists`);
             }
         }
-        let newGlobalList = {listName: globalList,lists:[]};
+        let newGlobalList = {listName: globalList, lists: [], proximityFunction: proximityFunction};
         this.lists.push(newGlobalList);
 
     }
-    removeAllEntries(){
+
+    removeAllEntries() {
 
     }
 
-    addEntry(globalList,entry){
-
+    /**
+     *
+     * @param globalList
+     * @return list or undefined if the list doesnt exist
+     */
+    getGlobalList(globalList) {
+        for (let list of this.lists){
+            if (list.listName === globalList) {
+                return list;
+            }
+        }
+        return undefined;
     }
+
+
+    addEntry(globalList, entry) {
+        let list = this.getGlobalList(globalList);
+        if (!list) {
+            console.warn(`tried to add entry ${entry} to non-existent global list: ${globalList}`);
+        } else {
+            for (let l of list.lists){
+                if (l.referenceElement === entry){
+                    console.warn(`a proximity list for entry ${entry} already exists.`);
+                    return;
+                }
+            }
+            let proxList = new ProximityList(this.proximityListSize, entry, list.proximityFunction);
+            list.lists.push(proxList);
+        }
+    }
+
     addNeighbor(list, neighbor) {
-
+        
     }
 
-    getAllProximityLists(globalList){
+    getAllProximityLists(globalList) {
 
     }
 
