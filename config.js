1cde4b8a3c2ed2f521f9d47cd2afc242de00fb17
hadi <modarres.zadeh@gmail.com>
2019-10-02 19:47:43 +1000

    config file

---
+++ config.js
@@ -0,0 +1,27 @@
+let CONFIG = {
+    NEIGHBOR_SIZE: 20,
+    SHUFFLE_SIZE: 5,
+    TICK_INTERVAL: 20000,
+    DEFAULT_SIGNALLING_SERVERS: [
+        {
+            "socket": {
+                "server": "http://localhost:12345"
+            },
+            "signallingApiBase": "http://localhost:12345"
+        },
+        {
+            "socket": {
+                "server": "http://localhost:12346"
+            },
+            "signallingApiBase": "http://localhost:12346"
+        }
+    ],
+    DEFAULT_BATCHING_DELAY_MS: 300,
+    DEFAULT_ICE_SERVERS: [
+        // The public Google STUN server
+        {urls: ['stun:stun.l.google.com:19302']},
+    ],
+    DEFAULT_CHANNEL_STATE_TIMEOUT_MS: 30000, DEFAULT_SIGNALLING_SERVER_RECONNECT_DELAY_MS: 5000
+};
+
+module.exports = CONFIG;
--- node.js
+++ node.js
@@ -13,32 +13,10 @@ const ListManager = require("./proximity/ListManager");
 let StatsRecorder = require("./stats/HTTPStatsRecorder");
 var EventEmitter = require("events").EventEmitter;
 let NodeStatsProbe = require("./stats/NodeStatsProbe");
-
-
+let config = require("./config");
 const constants = require("./constants");
-// let logger = {info: (msg)=>{console.info(msg)},log:()=>{},debug:()=>{},warn:()=>{},error:()=>{}};
+
 let logger = console;
-var DEFAULT_BATCHING_DELAY_MS = 300;
-var DEFAULT_SIGNALLING_SERVERS = [
-    {
-        "socket": {
-            "server": "http://localhost:12345"
-        },
-        "signallingApiBase": "http://localhost:12345"
-    },
-    {
-        "socket": {
-            "server": "http://localhost:12346"
-        },
-        "signallingApiBase": "http://localhost:12346"
-    }
-];
-var DEFAULT_ICE_SERVERS = [
-    // The public Google STUN server
-    {urls: ['stun:stun.l.google.com:19302']},
-];
-var DEFAULT_CHANNEL_STATE_TIMEOUT_MS = 30000;
-var DEFAULT_SIGNALLING_SERVER_RECONNECT_DELAY_MS = 5000;
 
 class Node extends EventEmitter{
     constructor() {
@@ -116,9 +94,9 @@ class Node extends EventEmitter{
         let timingService = new cyclonRtc.TimingService();
 
 //level 5
-        let signallingServerService = new cyclonRtc.StaticSignallingServerService(DEFAULT_SIGNALLING_SERVERS);
+        let signallingServerService = new cyclonRtc.StaticSignallingServerService(config.DEFAULT_SIGNALLING_SERVERS);
         let socketFactory = new cyclonRtc.SocketFactory();
-        let signallingServerSelector = new cyclonRtc.SignallingServerSelector(signallingServerService, persistentStorage, timingService, DEFAULT_SIGNALLING_SERVER_RECONNECT_DELAY_MS);
+        let signallingServerSelector = new cyclonRtc.SignallingServerSelector(signallingServerService, persistentStorage, timingService, config.DEFAULT_SIGNALLING_SERVER_RECONNECT_DELAY_MS);
 
 
 //level4
@@ -130,13 +108,13 @@ class Node extends EventEmitter{
 
 //level3
         let signallingService = new cyclonRtc.SocketIOSignallingService(signallingSocket, logger, httpRequestService, persistentStorage);
-        let peerConnectionFactory = new cyclonRtc.PeerConnectionFactory(rtcObjectFactory, logger, DEFAULT_ICE_SERVERS, DEFAULT_CHANNEL_STATE_TIMEOUT_MS);
+        let peerConnectionFactory = new cyclonRtc.PeerConnectionFactory(rtcObjectFactory, logger, config.DEFAULT_ICE_SERVERS, config.DEFAULT_CHANNEL_STATE_TIMEOUT_MS);
 
 
 //level 2
         let iceCandidateBatchingSignalling = new cyclonRtc.IceCandidateBatchingSignallingService(Utils.asyncExecService(),
-            signallingService, DEFAULT_BATCHING_DELAY_MS);
-        let channelFactory = new cyclonRtc.ChannelFactory(peerConnectionFactory, iceCandidateBatchingSignalling, logger,DEFAULT_CHANNEL_STATE_TIMEOUT_MS);
+            signallingService, config.DEFAULT_BATCHING_DELAY_MS);
+        let channelFactory = new cyclonRtc.ChannelFactory(peerConnectionFactory, iceCandidateBatchingSignalling, logger,config.DEFAULT_CHANNEL_STATE_TIMEOUT_MS);
         let shuffleStateFactory = new cyclonRtcComms.ShuffleStateFactory(logger, Utils.asyncExecService());
 
 
@@ -149,15 +127,15 @@ class Node extends EventEmitter{
 // level 0
 
         this.__cyclonNode = cyclon.builder(this.comms, this.bootStrap)
-            .withNumNeighbours(400)
+            .withNumNeighbours(config.NEIGHBOR_SIZE)
             .withMetadataProviders({
                     "clientInfo": () => {
                         return this.listManager.getAllLocalEntries();
                     },
                 }
             )
-            .withShuffleSize(400)
-            .withTickIntervalMs(20000)
+            .withShuffleSize(config.SHUFFLE_SIZE)
+            .withTickIntervalMs(config.TICK_INTERVAL)
             .build();
 
 
